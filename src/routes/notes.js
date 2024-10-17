1ac74a2503d8850f171134727615d48269f289e3
Gabriel Marini <gabrielmarini12@icloud.com>
2024-10-16 22:04:45 -0300

    adicionado rota de details

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -97,4 +97,18 @@ router.delete("/message/:id", (request, response) => {
   });
 });
 
+router.get("/details/:id", (request, response) => {
+  const { id } = request.params;
+
+  const note = notes.find((note) => note.id === id);
+
+  if (!note) {
+    return response.status(404).json({
+      message: "Recado não encontrado.",
+    });
+  }
+
+  response.status(200).json(note);
+});
+
 export default router;
